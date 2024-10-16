2ae42ca1d25ee55b336d25bbf3919b7734a1ed6f
EmanuelFacendini <efacendini@gmail.com>
2024-10-15 23:57:50 -0300

    react-native-meta2

--- app/(tabs)/about.js
+++ app/(tabs)/about.js
@@ -1,9 +1,9 @@
 import { Link } from "expo-router";
 import { Pressable, ScrollView, Text } from "react-native";
-import { HomeIcon } from "../components/Icons";
+import { HomeIcon } from "../../components/Icons";
 
 import { styled } from "nativewind";
-import { Screen } from "../components/Screen";
+import { Screen } from "../../components/Screen";
 
 const StyledPressable = styled(Pressable);
 
